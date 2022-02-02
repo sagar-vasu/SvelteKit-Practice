35f45f606254cfc40eb90edf5551c250cc672324
sagar-vasu <sagarvasu98@gmail.com>
2022-02-02 12:37:08 +0500

    Complete Website

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import global from "../global.css"
+    import Nav from "../components/nav.svelte"
+</script>
+
+<svelte:head>
+    <title>Movie DB</title>
+</svelte:head>
+
+<Nav />
+
+<slot />
---
+++ src/routes/index.svelte
@@ -0,0 +1,32 @@
+<script context="module" >
+    export async function load({fetch}){
+        const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${'YOUR API KEY'}&language=en-US&page=1`)
+
+        const data = await res.json();
+        if(res.ok){
+            return{
+                props:{popular:data.results}
+            }
+        }
+   }
+
+   import global from "../global.css"
+</script>
+
+<script>
+    import PopularMovies from "../components/popularMovies.svelte";
+    import SearchMovies from "../components/searchMovies.svelte";
+    import {fly} from "svelte/transition"
+    export let popular;
+</script>
+
+
+<section
+
+in:fly={{y:50, duration:500,delay:500}}
+out:fly={{y:50,duration:500}}
+>
+    <SearchMovies />
+    <PopularMovies movies = {popular} />
+</section>
+
